a9e1d68cde0a75276be5882feb618bbc93b6320f
VladimirMatos <vladi1417@gmail.com>
2021-09-18 23:35:00 -0400

    Commit with all code and TDD

--- Main.js
+++ Main.js
@@ -1,16 +1,11 @@
-
-const ISBNValidation = (isbn)=>{
-    const digits =isbn.replace(/-/gi,'');
-    
-
-    
-    /*if(digits == 13 ){
-        return ('Valid 13 digit');
-
-    }else if(digits == 10){
-        return ('Valid 10 digit');
-    }*/
-
+/*=> null -> throw ArgumentNullException
+=>  "" -> {}
+=>  024821858 -> {}
+=> "0-7167-0344-0" -> Valid 10 digit ISBN 
+=> "978-0-7167-0344-0" -> Valid 13 digit ISBN 
+*/
+const Validation10digits = (isbn10digits)=>{
+    const digits =isbn10digits.replace(/-/gi,'');
     const numbers = Array.from(digits);
     numbers.reverse();
     var count = 1;
@@ -20,17 +15,47 @@ const ISBNValidation = (isbn)=>{
         const arrayposition = numbers[i]*count
         sum += arrayposition;
         count ++;
-      }
+    }
 
     if(sum % 11 === 0 ){
-        return 'Valid';
+        return 'Valid 10 digit ISBN';
     } else{
-        return 'Invalid';
+        return 'invalid ISBN insert';      
     }
-    
 }
 
-console.log(ISBNValidation('0-7167-0344-0'));
+const Validation13digits = (isbn13digits) =>{
+    const digits =isbn13digits.replace(/-/gi,'');
+    const numbers = Array.from(digits);
+
+    const arrayposition = 
+        numbers[0] * 1 + numbers [1] * 3 + numbers[2] * 1 + numbers [3] * 3 + numbers[4] * 1 + numbers [5] * 3 +
+        numbers[6] * 1 + numbers [7] * 3 + numbers[8] * 1 + numbers [9] * 3 + numbers[10] * 1 + numbers [11] * 3 +
+        numbers[12] * 1
+     
+    if(arrayposition % 10 === 0){
+        return 'Valid 13 digit ISBN';
+    }else{
+        return 'invalid ISBN insert';
+    }
+}
+
+function Validation(isbn) {
+    if (isbn == null || !isbn){
+        return ('ArgumentNullException');
+    }if(isbn == " " || !isNaN(isbn)){
+        return " ";
+    }
+    const digits =isbn.replace(/-/gi,'');
+    const cantdigits = digits.length;
+    if(cantdigits == 10){
+        
+        return Validation10digits(digits);
+    }else if(cantdigits == 13){
+        return Validation13digits(digits);
+    }
+}
 
 
-module.exports = ISBNValidation;
+
+module.exports = Validation;
--- Test/maintest.test.js
+++ Test/maintest.test.js
@@ -1,19 +1,23 @@
-test('Kind of ISBN if ISBN have 13 digits',()=>{
-    expect(ISBNValidation('0-7167-0344-0')).toBe('Valid')
-   
+const Validation = require("../Main")
+
+test('Validation null input',()=>{
+    expect(Validation(null)).toBe('ArgumentNullException');
 })
 
-test('Kind of ISBN if ISBN have 10 digits',()=>{
-   
-    expect(ISBNValidation('0-7167-0344-0')).toBe('Valid 10 digit')
+
+test('Validation if input is empty',()=>{
+    expect(Validation(" ")).toBe(' ');
 })
 
-test('Test only string',()=>{
-   
-    expect(ISBNValidation(0-7167-0344-0)).toBe('Not valid')
+test('Validation if input is a int',()=>{
+    expect(Validation(5646546454)).toBe(' ');
 })
 
-test('Test sum numbers',()=>{
-   
-    expect(ISBNValidation("0-7167-0344-0")).toBe(187)
+
+test('Validation if input is a 10 digits ISBN',()=>{
+    expect(Validation('0-7167-0344-0')).toBe('Valid 10 digit ISBN');
+})
+
+test('Validation if input is a 13 digits ISBN',()=>{
+    expect(Validation('978-0-7167-0344-0')).toBe('Valid 13 digit ISBN');
 })
